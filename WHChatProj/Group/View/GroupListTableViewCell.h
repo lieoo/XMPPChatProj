5416c25bf4f9b920aff83e2cf9dcdd3a4fb59de0
lieo <1006100679@qq.com>
2017-04-21 14:32:35 +0800

    调整项目架构

---
+++ WHChatProj/Group/View/GroupListTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  GroupListTableViewCell.h
+//  WHChatProj
+//
+//  Created by 行政 on 17/4/21.
+//  Copyright © 2017年 lieo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GroupListTableViewCell : UITableViewCell
+
+@end
---
+++ WHChatProj/Group/View/GroupListTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  GroupListTableViewCell.m
+//  WHChatProj
+//
+//  Created by 行政 on 17/4/21.
+//  Copyright © 2017年 lieo. All rights reserved.
+//
+
+#import "GroupListTableViewCell.h"
+
+@implementation GroupListTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
